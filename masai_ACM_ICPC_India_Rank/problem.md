92aa7444bee05bce80ca0b1b00596c09a7694a22
avanishtatat <avanisht.at.at@gmail.com>
2023-05-01 09:33:36 +0530

    ACM ICPC India Rank

---
+++ masai_ACM_ICPC_India_Rank/problem.md
@@ -0,0 +1,23 @@
+## **ACM ICPC India Rank**
+
+ICPC is International Collegiate Programming Contest is an algorithmic programming contest for college students. You are provided list of countries in correct ranking. Your task is to write a program that prints the rank of India in ICPC.
+
+#### **Input Format**
+
+-
+
+#### **Output Format**
+
+- 
+
+#### **Sample Input**
+    input: 5
+           Russia
+           USA
+           Japan
+           China
+           India 
+
+#### **Sample Output**
+    output: 5
+
---
+++ masai_ACM_ICPC_India_Rank/solution.js
@@ -0,0 +1,26 @@
+function runProgram(input) {
+    var [ n, ...arr ]= input.trim().split("\n");
+    console.log(arr.indexOf("India") + 1)   
+  }
+  
+  if (process.env.USER === "") {
+    runProgram(``);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
+  
