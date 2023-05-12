ccf9bffeb69cda9268e7f97040d8225750c23576
manish-actyv <Manish.Tiwari@actyv.com>
2023-05-12 15:26:08 +0530

    reverse linked list

--- reverse_linked_list/solution.js
+++ reverse_linked_list/solution.js
@@ -10,5 +10,15 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
+    let prev = null;
+    let current = head;
     
+    while (current) {
+        const next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+    
+    return prev;
 };
